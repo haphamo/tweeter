306f18dfd034c6422cf19accc9c2ada8723d322c
Ha Pham <hapham0392@gmail.com>
2019-10-08 17:20:03 +0000

    Creating twitter clone project. Completed the separate css files in styles to stylize the header, nav, and new-tweet.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,7 @@
+//Remember to add the reference to this file and test (using a console.log perhaps?) that it is being loaded into the index.html file correctly.
+
+//Add a $(document).ready() function to your file to ensure the DOM has loaded.
+
+$(document).ready(function() {
+  // --- our code goes here ---
+});
