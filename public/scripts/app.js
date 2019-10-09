35c9a283ac7d45fe69d08d480861a531ab6dd791
Ha Pham <hapham0392@gmail.com>
2019-10-09 16:48:20 +0000

    implemeneted renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,37 +5,75 @@
  */
 
 //Implement a createTweetElement function, create first template first
-
-/*
-<section class="tweet-container">
-            <header><img class="avatar" src="https://i.imgur.com/73hZDYK.png"> <span id="name">Name</span> 
-              <span id="handle" >@handle</span>
-            </header>
-              <p>SOME CONTENT HERE</p>
-            <footer class="footer">10 days ago 
-              <div id="bottom-right"><i class="fas fa-flag" id="flag"></i><i class="fas fa-retweet" id="retweet"></i><i class="fas fa-heart" id="heart"></i>
-              </div>
-            </footer>
-          </section>
-*/
-
-
-const createTweetElement = function (tweet) {
-  
-    const markup = `
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Ha",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@haphamo" },
+    "content": {
+      "text": "Hello LightHouseLabs"
+    },
+    "created_at": 736874876838
+  }
+]
+const createTweetElement = function (tweetData) {
+    const tweetMarkup = `
     <section class="tweet-container">
-      <header><img class="avatar" src="https://i.imgur.com/73hZDYK.png">
-        <span id="name">${tweetData[user].name}</span> 
-        <span id="handle">${tweetData[user].handle}</span>
+      <header><img class="avatar" src="${tweetData.user.avatars}">
+        <span id="name">${tweetData.user.name}</span> 
+        <span id="handle">${tweetData.user.handle}</span>
       </header>
-      <p>${tweetData.content}</p>
-      <footer class="footer">${tweetData.created_at} 
+      <p>${tweetData.content.text}</p>
+      <footer class="footer">${new Date(tweetData.created_at)} 
         <div id="bottom-right"><i class="fas fa-flag" id="flag"></i><i class="fas fa-retweet" id="retweet"></i><i class="fas fa-heart" id="heart"></i>
         </div>
       </footer>
     </section>
     `;
-  return markup;
+  return tweetMarkup;
 }
 
-document.body.innerHTML = markup;
+const renderTweets = function(tweetData) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  let tweets = "";
+  for (tweet of tweetData) {
+    tweets += createTweetElement(tweet);
+  } 
+  $('.container').append(tweets);
+}
+
+
+$(document).ready(function() {
+  // const tweet = createTweetElement(tweetData);
+  // console.log(tweet); // to see what it looks like
+  // $('.container').append(tweet);
+  renderTweets(tweetData);
+
+});
+    
+    
